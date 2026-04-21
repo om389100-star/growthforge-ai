b8707cdef987254c0d3ad1adbf947342f8b34d3c
Mohammed Omer <mdomer1929@gmail.com>
2026-04-21 19:44:31 +0400

    add chatbot script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
